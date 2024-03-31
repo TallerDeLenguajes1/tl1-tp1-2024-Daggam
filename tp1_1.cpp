c3bd622d1c2a7a5cdbc24a870a30386117d68ca0
Benja <benjaminvilla409@gmail.com>
2024-03-30 22:12:19 -0300

    Punteros en c++ y solucione un problema con el .gitignore

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main(){
+    int a = 10;
+    int *ap = &a;
+    printf("El contenido del puntero: %d\n", *ap);
+    printf("Direccion de memoria almacenada por el puntero: %p\n",ap);
+    printf("Direccion de memoria de la variable: %p\n",&a);
+    printf("Direccion de memoria del puntero: %p\n",&ap);
+    printf("Tamanio de memoria utilizado: %d bytes\n",sizeof(a));
     std::cout<<"Hola mundo!";
     return 0;
 }
